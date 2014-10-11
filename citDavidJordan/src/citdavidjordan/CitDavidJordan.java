1f5f822b49e0964c5ec3c05ec92fc93f8f12ac4a
THERIAULT <THERIAULT@THERIAULT-PC>
2014-10-11 11:11:14 -0600

    added tests for Actor, Game, & Path Classes

--- citDavidJordan/src/citdavidjordan/CitDavidJordan.java
+++ citDavidJordan/src/citdavidjordan/CitDavidJordan.java
@@ -6,7 +6,10 @@
 
 package citdavidjordan;
 
+import citdavidjordan.model.Actor;
 import citdavidjordan.model.Player;
+import citdavidjordan.model.Game;
+import citdavidjordan.model.Path;
 
 /**
  *
@@ -18,6 +21,7 @@ public class CitDavidJordan {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //***test Player class***
         Player playerOne = new Player();
         
         playerOne.setName("Steven Spielberg");
@@ -25,5 +29,36 @@ public class CitDavidJordan {
         
         String playerInfo = playerOne.toString();
         System.out.println(playerInfo);
+        
+        //***test Game Class***
+        Game gameOne = new Game();
+        
+        gameOne.setPlayer(playerOne);
+        gameOne.setTriviaCompleted("testTrivia");
+        gameOne.setHomeworkCompleted("testHomework");
+        gameOne.setBullyWatchYorN(true);
+        gameOne.setSaveDate("10/10/2014");
+        
+        String gameInfo = gameOne.toString();
+        System.out.println(gameInfo);
+        
+        //***test Path Class***
+        Path pathOne = new Path();
+        
+        pathOne.setPathNo(4);
+        
+        String pathInfo = pathOne.toString();
+        System.out.println(pathInfo);
+        
+         //***test Actor class***
+        Actor actorOne = new Actor();
+        
+        actorOne.setActorNo(1);
+        
+        String actorInfo = actorOne.toString();
+        System.out.println(actorInfo);
+        
     } 
+    
+    
 }
