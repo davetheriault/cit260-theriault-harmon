aa48fb24f8a056b8c9210719fadc8b22ba1ab305
THERIAULT <THERIAULT@THERIAULT-PC>
2014-11-01 23:14:42 -0600

    Finished (mostly) RPSGameView

--- citDavidJordan/src/citdavidjordan/control/RPSGameControl.java
+++ citDavidJordan/src/citdavidjordan/control/RPSGameControl.java
@@ -28,31 +28,31 @@ public class RPSGameControl {
  	int versus = rand.nextInt(3) + 1;
 
 	if ( choice == versus ) {
-            return "It's a tie! Play again.";
+            return "Rocky chose the same thing. \nIt's a tie! Play again.";
         }
 	
         if ( (choice == 1) && (versus == 2) ) { 
-            return "Paper covers rock. You lose.";
+            return "Rocky chose Paper. \nPaper covers rock. You lose.";
         }
 
 	if ( (choice == 1) && (versus == 3) ) {
-            return "Rock smashes scissors. You win!";
+            return "Rocky chose scissors. \nRock smashes scissors. You win!";
         }
 
 	if ( (choice == 2) && (versus == 1) ) { 
-            return "Paper covers rock. You win!";
+            return "Rocky chose Rock. \nPaper covers rock. You win!";
         }
 
 	if ( (choice == 2) && (versus == 3) ) { 
-            return "Scissors cut paper. You lose.";
+            return "Rocky chose Scissors. \nScissors cut paper. You lose.";
         }
 
 	if ( (choice == 3) && (versus == 1) ) { 
-            return "Rock smashes scissors. You lose.";
+            return "Rocky chose Rock. \nRock smashes scissors. You lose.";
         }
 
 	if ( (choice == 3) & (versus == 2) ) { 
-            return "Scissors cut paper. You win!";
+            return "Rocky chose Paper. \nScissors cut paper. You win!";
         }
         
         return null;
