627ecfc8bcbeefddc9d008760ddbff199767d268
THERIAULT <THERIAULT@THERIAULT-PC>
2014-12-11 13:48:14 -0700

    shell game

--- citDavidJordan/src/citdavidjordan/view/Scene3ShellView.java
+++ citDavidJordan/src/citdavidjordan/view/Scene3ShellView.java
@@ -5,8 +5,13 @@
  */
 package citdavidjordan.view;
 
+import citdavidjordan.control.Scene3ShellControl;
 import citdavidjordan.exceptions.MenuControlException;
 import citdavidjordan.model.Actor;
+import java.io.IOException;
+import static java.lang.Integer.parseInt;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -48,7 +53,59 @@ public class Scene3ShellView extends View{
 
     @Override
     public void doAction(String value) throws MenuControlException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        try {
+            value = value.toUpperCase();
+            
+            switch (value) {
+                
+                case "Y":
+                    this.console.println("\nShelton: \"Alright. Do you you know how to play?\" \n"
+                            + "(Y/N)?");
+                    String x = this.keyboard.readLine();
+                    if (x.toUpperCase().equals("Y")) { this.doAction("Y2");}
+                    else if (x.toUpperCase().equals("N")) { this.doAction("HOWTO");}
+                    else { throw new IOException("Invalid Response.");}
+                    break;
+                    
+                case "N":
+                    this.console.println("\nShelton: \"Don't got what it takes, eh? Suit yourself.\"\n"
+                            + "(Press <Enter> to return to the map.)");
+                    this.doAction("QUIT");
+                    break;
+                    
+                case "HOWTO":
+                    this.console.println("\nSheldon: \"It's simple. I have 3 cups and 1 ball.\n"
+                            + "\t I'll place the ball under the center cup to start. \n"
+                            + "\t Then I will rearrange the cups. You just keep your eye\n"
+                            + "\t on the cup with the ball under it, and tell me wear it\n"
+                            + "\t is, and you win.\"");
+                    this.console.println("\n(To represent the cup game, you must picture three separate)\n"
+                                         + "(cup positions - LEFT, CENTER, & RIGHT.  For each time     )\n"
+                                         + "(Sheldon shuffles the cups, either the words LEFT or RIGHT )\n"
+                                         + "(will display on the screen, representing the movement of  )\n"
+                                         + "(the center cup which contains the ball. Based on the move-)\n"
+                                         + "(ment of the center cup, determine its final position.     )");
+                    this.console.println("(Press <Enter> to continue.)");
+                    this.keyboard.readLine();
+                    this.doAction("Y2");
+                    break;
+                    
+                case "Y2":
+                    MenuGameView mgv = new MenuGameView(); mgv.displayInventory(this.console);
+                    this.console.println("\nSheldon: \"So, what kind of marbles are you betting?");
+                    String mType = this.keyboard.readLine().trim().toLowerCase();
+                    this.console.println("Sheldon: \"And how many?\"");
+                    int mNo = parseInt(this.keyboard.readLine().trim());
+                    
+                    this.console.println("\nSheldon: \"Ok. Let's do this.\"\n\n"
+                            + "Sheldon puts the ball under the center cup. It moves:\n");
+                    Scene3ShellControl shell = new Scene3ShellControl(); 
+                    int ans = shell.shellGame();
+            }
+        } catch (IOException ex) {
+            ErrorView.display(this.getClass().getName(), ex.getMessage());
+        }
     }
     
 }
