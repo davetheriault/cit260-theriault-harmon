588e8a2c2e48f640ede680f1a44e2221201b0651
Deann_2 <Deann_2@Deann.home>
2014-12-11 17:03:22 -0700

    Lesson 11 Naoto View and Exception

---
+++ citDavidJordan/src/citdavidjordan/exceptions/Scene8NaotoException.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package citdavidjordan.exceptions;
+
+/**
+ *
+ * @author Deann_2
+ */
+public class Scene8NaotoException extends Exception{
+
+    public Scene8NaotoException() {
+    }
+
+    public Scene8NaotoException(String message) {
+        super(message);
+    }
+
+    public Scene8NaotoException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public Scene8NaotoException(Throwable thrwbl) {
+        super(thrwbl);
+    }
+
+    public Scene8NaotoException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+    
+}
